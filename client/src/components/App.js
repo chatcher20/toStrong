98a11f9a414b87eae2f83429f16346494f65136a
John Chia <john.chia0106@gmail.com>
2022-03-21 15:25:26 -0700

    reformat files

---
+++ client/src/components/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Navigation from './Navigation';
+import Main from './Main';
+
+const App = () => {
+  return (
+    <div className='app'>
+    <h1>🏋️.toStrong</h1>
+    <Main />
+      <footer>
+
+      </footer>
+    </div>
+  )
+};
+
+export default App;
