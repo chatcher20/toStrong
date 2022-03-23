209c3611d405a2c83b5c6b7a45d4028d07a27846
Chris Hatcher <hatcherchris20@gmail.com>
2022-03-23 21:28:15 +0000

    changed integer to number

--- server/src/entity/Planned_Workout.ts
+++ server/src/entity/Planned_Workout.ts
@@ -1,6 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm"
 import { Program } from "../entity/Program"
-import { Exercise } from "../entity/Exercise"
 
 @Entity()
 export class Planned_Workout {
@@ -9,7 +8,7 @@ export class Planned_Workout {
   id: number
 
   @Column()
-  day: integer
+  day: number            // day 1 has an assigned exercises of squat, bench, deadlift for example. (An object where the keys are the exercise)
 
   @Column()
   exercise_order: string
