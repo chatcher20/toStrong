9c3357c41493e77c0760e289dcee98ba2234a274
leepavelich <leepavelich@MacBook-Pro.local>
2022-03-23 10:55:24 -0600

    Add type to round fnc

--- client/src/helpers/bmi-macros.js
+++ client/src/helpers/bmi-macros.js
@@ -7,7 +7,7 @@ const fakeStats = {
   weight_change: "bulk",
 };
 
-round = (value, precision) => {
+const round = (value, precision) => {
   const multiplier = Math.pow(10, precision || 0);
   return Math.round(value * multiplier) / multiplier;
 };
