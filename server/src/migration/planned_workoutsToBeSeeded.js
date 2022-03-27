7cb18009db033c36f5165ac79918ab801748cfcf
leepavelich <leepavelich@MacBook-Pro.local>
2022-03-26 19:23:29 -0600

    Basic LP planned_workouts seeded

--- server/src/migration/planned_workoutsToBeSeeded.js
+++ server/src/migration/planned_workoutsToBeSeeded.js
@@ -1,16 +1,25 @@
-const planned_workoutsToBeSeeded = []
-for (let i = 0; i<5; i++) {
-  planned_workoutsToBeSeeded.push(
-    {
-      // exercise: exercises.filter(e => e.name === "Bench Press")[0]
-      day: i,
-      exercise_order: 4,
-      exercise_name: "Bench Press",
-      program_name: "BasicLP"
+const planned_workoutsToBeSeeded = [];
+for (let day = 1; day <= 36; day++) {
+  for (let exercise_order = 1; exercise_order <= 3; exercise_order++) {
+    const isAWorkout = day % 2;
+
+    if (exercise_order === 1) exercise_name = "Squat";
+    if (exercise_order === 2) {
+      if (isAWorkout) exercise_name = "Overhead Press";
+      else exercise_name = "Bench Press";
     }
-  )
-};
+    if (exercise_order === 3) {
+      if (isAWorkout) exercise_name = "Deadlift";
+      else exercise_name = "Chin Up";
+    }
 
-module.exports = { planned_workoutsToBeSeeded }
+    planned_workoutsToBeSeeded.push({
+      day,
+      exercise_order,
+      exercise_name,
+      program_name: "basic-lp",
+    });
+  }
+}
 
-
+module.exports = { planned_workoutsToBeSeeded };
