d28aca5d29a24a90967a4b4ec3f54446f7070d1e
Lee Pavelich <leepavelich@users.noreply.github.com>
2022-03-28 20:39:40 -0600

    Update actual_workoutsToBeSeeded.js

--- server/src/migration/actual_workoutsToBeSeeded.js
+++ server/src/migration/actual_workoutsToBeSeeded.js
@@ -3,7 +3,7 @@ const actual_workoutsToBeSeeded = [
     user_id: 1,
     program_name: "Basic-LP",
     day: 1,
-    exercisesCompleted: { "Squat": true, "Bench Press": true, "Deadlidt": false }
+    exercisesCompleted: { "Squat": true, "Bench Press": true, "Deadlift": false }
   },
 ]
 
