c2d8d182ccc50b7b7771a90ab06c447b892ed469
leepavelich <leepavelich@MacBook-Pro.local>
2022-03-30 11:12:03 -0600

    Style program list tags

--- client/src/components/ProgramListItem.js
+++ client/src/components/ProgramListItem.js
@@ -4,19 +4,38 @@ import icon from "../images/icon.png";
 import Button from "../components/Button";
 
 export default function ProgramListItem(props) {
+  const tag = (text) => {
+    if (text === "Full-body") {
+      return <div class="tag is-warning is-medium has-text-black">{text}</div>;
+    }
+    if (text === "Upper-body") {
+      return <div class="tag is-info is-medium has-text-white">{text}</div>;
+    }
+    if (text === "Lower-body") {
+      return <div class="tag is-dark is-medium has-text-white">{text}</div>;
+    }
+    if (text === "Barbell") {
+      return <div class="tag is-danger is-medium has-text-white">{text}</div>;
+    }
+    if (text === "Commercial Gym") {
+      return <div class="tag is-link is-medium has-text-white">{text}</div>;
+    }
+    if (text === "Bodyweight") {
+      return <div class="tag is-light is-medium has-text-black">{text}</div>;
+    }
+  };
+
   return (
     <div>
       <div className="card">
         <div className="card-content">
           <div className="media">
-
             <div className="media-content-programs">
-
               <div className="media-left">
                 <figure className="image is-48x48">
                   <img src={icon} alt="toStrong icon" />
                 </figure>
-                <div className='program-title'>
+                <div className="program-title">
                   <p className="title is-5">{props.name}</p>
                 </div>
               </div>
@@ -30,9 +49,11 @@ export default function ProgramListItem(props) {
               </div>
             </div>
             <div className="library-tag">
-            <div class="tag is-success has-text-black">{props.modality}</div>
-            <div class="tag is-info has-text-dark">{props.type}</div>
-            <div class="tag is-link">{`Duration: ${props.duration} Days`}</div>
+              {tag(props.modality)}
+              {tag(props.type)}
+            </div>
+            <div>
+              <strong>Duration</strong>:{` ${props.duration} workouts`}
             </div>
           </div>
 
